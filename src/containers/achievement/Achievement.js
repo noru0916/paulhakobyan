be6e0e229e6ccdeaa786df6ef6379af024ef4c70
unknown <noru0916@gmail.com>
2023-04-26 06:15:32 -0400

    clear repo

--- src/containers/achievement/Achievement.js
+++ src/containers/achievement/Achievement.js
@@ -9,12 +9,12 @@ export default function Achievement() {
   //   win.focus();
   // }
   return (
-    <div className="main" id="achievements">
-      <div className="achievement-main-div">
+    <div className="main" id="achievements">`
+      <div className="achievement-main-div">`
         <div className="achievement-header">
           <h1 className="heading achievement-heading">
             {achievementSection.title}
-          </h1>
+          </h1> 
           <p className="subTitle achievement-subtitle">
             {achievementSection.subtitle}
           </p>
